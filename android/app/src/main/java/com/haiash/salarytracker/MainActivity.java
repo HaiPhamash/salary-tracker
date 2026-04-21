de82926af3b6d93f88cf9e0606b7ad03a50606fb
Phamhaiash <phamsunghai@gmail.com>
2026-04-21 21:22:20 +0900

    Update project

--- android/app/src/main/java/com/haiash/salarytracker/MainActivity.java
+++ android/app/src/main/java/com/haiash/salarytracker/MainActivity.java
@@ -1,5 +1,13 @@
 package com.haiash.salarytracker;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDelegate;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+    }
+}
